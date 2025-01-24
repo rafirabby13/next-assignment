e7522062923198b071cf6c9cd9092bb27d55c7b3
rafi <rafiahmedrabby282@gmail.com>
2025-01-24 10:07:02 +0600

    added cards

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -4,13 +4,14 @@ import React from "react";
 const Posts = ({ posts }) => {
 //   console.log(posts);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-3
+    ">
       {
-        posts.map((post,i)=><div key={i}  className="card bg-base-100 w-96 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">{post.title}</h2>
+        posts.map((post,i)=><div key={i}  className=" bg-base-100 shadow-xl">
+            <div className="p-3 space-y-4">
+              <h2 className="text-2xl font-bold">{post.title}</h2>
               <p>{post.body}</p>
-              <div className="card-actions justify-end">
+              <div className="">
                 <Link href={`/post/${post.id}`} className="btn btn-primary">Details</Link>
               </div>
             </div>
