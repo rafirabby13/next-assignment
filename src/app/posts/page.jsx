e973e372897405b3f8fcf97442117311b89bfae7
rafi <rafiahmedrabby282@gmail.com>
2025-01-24 19:20:04 +0600

    added blog

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -9,15 +9,15 @@ const Posts = ({ posts }) => {
   }
   return (
     <div
-      className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-1 md:gap-3 py-10 md:mx-20 rounded-md
+      className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-1 md:gap-2 lg:gap-3 py-10 md:mx-20 rounded-md
     "
     >
       {posts?.map((post, i) => (
         <div key={i} className=" bg-white shadow-xl rounded-md">
-          <div className="p-2 md:p-9 space-y-6 ">
+          <div className="p-2  xl:p-9 space-y-6 ">
             <div className="space-y-6 flex flex-col justify-between h-full">
-              <Link href={`/post/${post.id}`} className="text-lg md:text-2xl font-bold hover:underline">{post.title}</Link>
-              <p className="text-sm md:text-lg">{post.body}</p>
+              <Link href={`/blog/${post.id}`} className="text-lg lg:text-2xl font-bold hover:underline">{post.title}</Link>
+              <p className="text-sm lg:text-lg">{post.body}</p>
             </div>
             {/* <div className="">
               <Link  className="btn btn-outline btn-sm text-[#09122C]">
