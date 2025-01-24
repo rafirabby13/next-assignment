1e733e86f481f55b3db58fc59c1e80da7c96de7a
rafi <rafiahmedrabby282@gmail.com>
2025-01-24 16:49:42 +0600

    added user sign in and sign up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,45 @@
+"use server";
 import Link from "next/link";
 import React from "react";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  console.log(user);
   return (
     <div>
-      <div className=" bg-base-100 flex gap-3  justify-center text-2xl font-semibold py-10">
+      <div className=" bg-base-100 flex gap-3  justify-center text-2xl font-semibold py-10 items-center">
         <p className="">
           <Link href="/">Home</Link>
         </p>
         <p>
           <Link href="/profile">Profile</Link>
         </p>
-        <LoginLink className="btn btn-accent" postLoginRedirectURL="/profile">
-          Sign in
-        </LoginLink>
-        <RegisterLink
-          postLoginRedirectURL="/profile"
-          className="btn btn-accent"
-        >
-          Sign up
-        </RegisterLink>
+        {user ? (
+          <LogoutLink className="btn btn-accent">Log out</LogoutLink>
+        ) : (
+          <>
+            <LoginLink
+              className="btn btn-accent"
+              postLoginRedirectURL="/profile"
+            >
+              Sign in
+            </LoginLink>
+            <RegisterLink
+              postLoginRedirectURL="/profile"
+              className="btn btn-accent"
+            >
+              Sign up
+            </RegisterLink>
+          </>
+        )}
       </div>
     </div>
   );
