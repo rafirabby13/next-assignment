dd39b8681c6520ff05c95b40540e93da8b787304
rafi <rafiahmedrabby282@gmail.com>
2025-01-24 17:50:15 +0600

    added user protected route

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,23 @@
+"use client";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 import React from "react";
 
 const page = () => {
+      const { isAuthenticated, isLoading } = useKindeBrowserClient();
+      if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
-      <div className="text-4xl text-center font-semibold border-2 p-4 shadow-2xl">
+      {
+        isAuthenticated ? <div className="text-4xl text-center font-semibold border-2 p-4 shadow-2xl">
         "Welcome to your profile!"
-      </div>
+      </div>:
+      <div className="text-4xl text-center">
+      You have to <LoginLink className="btn btn-accent">Login</LoginLink> to see this page
+    </div>
+      }
     </div>
   );
 };
